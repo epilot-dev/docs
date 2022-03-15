3655159e38cea20746517de0d220251a5dc3621c
Sumanth Kanakala <s.kanakala@epilot.cloud>
2022-03-15 16:47:49 +0530

    implemented graphl docs

---
+++ graphql-markdown.config.js
@@ -0,0 +1,21 @@
+module.exports = [
+  ...[
+    {
+      id: 'customer-portal',
+      url: 'https://customer-portal-api.dev.sls.epilot.io/v1/graphql',
+      homepage: 'static/graphql/customer-portal.md',
+    },
+  ].map((schema) => [
+    '@edno/docusaurus2-graphql-doc-generator',
+    {
+      id: schema.id,
+      schema: schema.url,
+      rootPath: '.',
+      baseURL: `/graphql/${schema.id}`,
+      homepage: schema.homepage,
+      loaders: {
+        UrlLoader: '@graphql-tools/url-loader',
+      },
+    },
+  ]),
+];
---
+++ static/graphql/customer-portal.md
@@ -0,0 +1,12 @@
+---
+id: schema
+slug: /customer-portal
+title: Customer Portal
+sidebar_position: 1
+hide_table_of_contents: true
+pagination_next: null
+pagination_prev: null
+sidebar_class_name: navbar__toggle
+---
+
+This documentation has been automatically generated using [Customer Portal Graphql](https://customer-portal-api.dev.sls.epilot.io/v1/graphql) endpoint.
