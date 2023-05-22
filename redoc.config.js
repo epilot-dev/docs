ea963d1506d35bdebb35652a4b7b5e2660cde448
Viljami <viljami@viljami.io>
2023-05-22 10:27:23 +0200

    Separate API specs to a different file

---
+++ redoc.config.js
@@ -0,0 +1,107 @@
+module.exports.specs = [
+  {
+    layout: { title: 'Access Token API' },
+    routePath: '/api/access-token',
+    specUrl: 'https://docs.api.epilot.io/access-token.yaml',
+  },
+  {
+    layout: { title: 'User API' },
+    routePath: '/api/user',
+    specUrl: 'https://docs.api.epilot.io/user.yaml',
+  },
+  {
+    layout: { title: 'Organization API' },
+    routePath: '/api/organization',
+    specUrl: 'https://docs.api.epilot.io/organization-v2.yaml',
+  },
+  {
+    layout: { title: 'Entity API' },
+    routePath: '/api/entity',
+    specUrl: 'https://docs.api.epilot.io/entity.yaml',
+  },
+  {
+    layout: { title: 'Submission API' },
+    routePath: '/api/submission',
+    specUrl: 'https://docs.api.epilot.io/submission-api.yaml',
+  },
+  {
+    layout: { title: 'Pricing API' },
+    routePath: '/api/pricing',
+    specUrl: 'https://docs.api.epilot.io/pricing-api.yaml',
+  },
+  {
+    layout: { title: 'Query API' },
+    routePath: '/api/query',
+    specUrl: 'https://docs.api.epilot.io/query.yaml',
+  },
+  {
+    layout: { title: 'Webhooks API' },
+    routePath: '/api/webhooks',
+    specUrl: 'https://docs.api.epilot.io/webhooks-howto.yaml',
+  },
+  {
+    layout: { title: 'Permissions API' },
+    routePath: '/api/permissions',
+    specUrl: 'https://docs.api.epilot.io/permissions.yaml',
+  },
+  {
+    layout: { title: 'Message API' },
+    routePath: '/api/message',
+    specUrl: 'https://docs.api.epilot.io/message.yaml',
+  },
+  {
+    layout: { title: 'File API' },
+    routePath: '/api/file',
+    specUrl: 'https://docs.api.epilot.io/file.yaml',
+  },
+  {
+    layout: { title: 'Journey API' },
+    routePath: '/api/journey',
+    specUrl: 'https://docs.api.epilot.io/journey-config.yaml',
+  },
+  {
+    layout: { title: 'Automation API' },
+    routePath: '/api/automation',
+    specUrl: 'https://docs.api.epilot.io/automation.yaml',
+  },
+  {
+    layout: { title: 'Document API' },
+    routePath: '/api/document',
+    specUrl: 'https://docs.api.epilot.io/document.yaml',
+  },
+  {
+    layout: { title: 'Template Variables API' },
+    routePath: '/api/template-variables',
+    specUrl: 'https://docs.api.epilot.io/template-variables.yaml',
+  },
+  {
+    layout: { title: 'Customer Portal API' },
+    routePath: '/api/customer-portal',
+    specUrl: 'https://docs.api.epilot.io/customer-portal.yaml',
+  },
+  {
+    layout: { title: 'Notification API' },
+    routePath: '/api/notification',
+    specUrl: 'https://docs.api.epilot.io/svc-notification-api.yaml',
+  },
+  {
+    layout: { title: 'Workflow Definition API' },
+    routePath: '/api/workflow-definition',
+    specUrl: 'https://docs.api.epilot.io/workflows-definition.yaml',
+  },
+  {
+    layout: { title: 'Workflow Execution API' },
+    routePath: '/api/workflow-execution',
+    specUrl: 'https://docs.api.epilot.io/workflows-execution.yaml',
+  },
+  {
+    layout: { title: 'Partner API' },
+    routePath: '/api/partner',
+    specUrl: 'https://docs.api.epilot.io/partner-directory-api.yaml',
+  },
+  {
+    layout: { title: 'Internal Auth API' },
+    routePath: '/api/internal-auth',
+    specUrl: 'https://docs.api.epilot.io/internal-auth.yaml',
+  },
+]
